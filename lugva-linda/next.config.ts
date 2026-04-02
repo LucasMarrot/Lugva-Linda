9934fb6b9e91b7ba4d2420234c66a4f3eff80e9e
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2026-04-02 19:15:39 +0200

    feat: add allowedDevOrigins configuration for development environment

--- lugva-linda/next.config.ts
+++ lugva-linda/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 import path from 'node:path';
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.1.192'],
   turbopack: {
     root: path.join(__dirname),
   },
