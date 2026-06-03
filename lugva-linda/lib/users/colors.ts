8a101867b5e6361f89fdfafda37d7cba195651c5
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2026-06-03 22:04:46 +0200

    feat: First thinking about new colors

--- lugva-linda/lib/users/colors.ts
+++ lugva-linda/lib/users/colors.ts
@@ -9,6 +9,30 @@ export const USER_COLOR_OPTIONS = [
   '#EA580C',
   '#DC2626',
   '#E11D48',
+  '#854D0E', // Bronze / Moutarde foncé (L'alternative au jaune)
+  '#5D4037', // Marron Mocha
+  '#0F6C80', // Bleu Pétrole (L'alternative au vert d'eau)
+  '#4D7C0F', // Vert Olive
+  '#1E3A8A', // Bleu Nuit
+  '#4C1D95', // Violet Impérial
+  '#701A75', // Prune / Aubergine
+  '#831843', // Cassis
+  '#3F3F46', // Gris Carbone
+  '#0F172A', // Encre / Slate ultra-profond
+  '#1B1B1B', // Noir profond
 ] as const;
+
+// export const USER_COLOR_OPTIONS = [
+//   '#854D0E', // Bronze / Moutarde foncé (L'alternative au jaune)
+//   '#5D4037', // Marron Mocha
+//   '#0F6C80', // Bleu Pétrole (L'alternative au vert d'eau)
+//   '#4D7C0F', // Vert Olive
+//   '#1E3A8A', // Bleu Nuit
+//   '#4C1D95', // Violet Impérial
+//   '#701A75', // Prune / Aubergine
+//   '#831843', // Cassis
+//   '#3F3F46', // Gris Carbone
+//   '#0F172A', // Encre / Slate ultra-profond
+// ] as const;
 
 export type UserColorOption = (typeof USER_COLOR_OPTIONS)[number];
